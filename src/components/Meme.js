9248642b930cd70b792a16abb543980d29c849e0
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-25 10:16:30 +0530

    Added Memes data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import memesData from "../memesData.js"
 
 const Meme = () => {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        // console.log(randomNumber)
+
+        const url = memesArray[randomNumber].url
+        // console.log(url);    
+        
+    }
+
   return (
     <main>
-        <form className='form'>
-            <input type = "text" placeholder='Top text' className='form--input'/>
-            <input type = "text" placeholder='Bottom text' className='form--input'/>
-            <button className='form--button'>Get a new meme image 🖼</button>
-        </form>
+        <div className="form">
+        <input 
+            type="text"
+            placeholder="Top text"
+            className="form--input"
+        />
+        <input 
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+        />
+        <button 
+            className="form--button"
+            onClick  = {getMemeImage}
+        >
+            Get a new meme image 🖼
+        </button>
+        </div>
     </main>
   )
 }
